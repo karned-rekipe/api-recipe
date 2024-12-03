941f54abb9f366a51a83437ff4b39af4b5753c29
Killian KOPP <killiankopp@gmail.com>
2024-12-03 16:44:49 +0100

    ménage
    Signed-off-by: Killian KOPP <killiankopp@gmail.com>

--- models/ingredient_model.py
+++ models/ingredient_model.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel, Field, HttpUrl
-from typing import Optional, List
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
 
 class Ingredient(BaseModel):
     name: str
--- models/step_model.py
+++ models/step_model.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel, Field, HttpUrl
-from typing import Optional, List
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
 
 class Step(BaseModel):
     step_number: int
