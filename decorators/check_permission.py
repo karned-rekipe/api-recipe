40b7c55061c22585d90b25c92928d70a175f4b9c
Killian KOPP <killiankopp@gmail.com>
2025-05-04 11:35:16 +0200

    refactor database middleware: update header key retrieval to use 'X-License-Key'

--- decorators/check_permission.py
+++ decorators/check_permission.py
@@ -3,14 +3,34 @@ from fastapi import status, Request, HTTPException
 from functools import wraps
 from typing import List
 
+from config.config import API_NAME
 
-def check_roles( list_roles: list, permissions: List[str] ) -> None:
-    if not any(perm in list_roles for perm in permissions):
+
+def check_roles( list_roles: dict, permissions: List[str] ) -> None:
+    required_permissions = []
+    for perm in permissions:
+        if "/" in perm:
+            required_permissions.append(perm)
+        else:
+            required_permissions.append(f"{API_NAME}/{perm}")
+
+    formatted_roles = []
+    for api, roles_data in list_roles.items():
+        if isinstance(roles_data, list):
+            for role in roles_data:
+                formatted_roles.append(f"{api}/{role}")
+        elif isinstance(roles_data, dict) and 'roles' in roles_data:
+            for role in roles_data['roles']:
+                formatted_roles.append(f"{api}/{role}")
+        else:
+            formatted_roles.append(f"{api}/roles")
+
+    if not any(perm in formatted_roles for perm in required_permissions):
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Insufficient permissions ! "
-                   "Need : " + ", ".join(permissions) +
-                   " / Got : " + ", ".join(list_roles)
+                   "Need : " + ", ".join(required_permissions) +
+                   " / Got : " + ", ".join(formatted_roles)
         )
 
 
