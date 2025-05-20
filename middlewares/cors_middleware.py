256700db60c41b9f126c8b46edbb5b6a9e2ac955
Killian KOPP <killiankopp@gmail.com>
2025-05-20 13:21:34 +0200

    Add CORS middleware to main application and update tests

---
+++ middlewares/cors_middleware.py
@@ -0,0 +1,15 @@
+import logging
+from starlette.middleware.cors import CORSMiddleware as StarletteCorsMW
+
+class CORSMiddleware(StarletteCorsMW):
+    def __init__(self, app):
+        logging.info("Initializing CORSMiddleware")
+        super().__init__(
+            app=app,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+            expose_headers=["Content-Type", "X-License-Key", "Authorization"],
+            max_age=600
+        )
