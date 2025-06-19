0e2d69973733e9753bcba671e38ffa293e399b3a
sheligon <heligon.seb@gmail.com>
2025-06-19 16:44:03 +0200

    ajout du package shared

--- decorators/check_permission.py
+++ decorators/check_permission.py
@@ -4,7 +4,7 @@ from typing import List
 
 from services.permission_service import check_user_has_permissions
 
-def check_permissions(permissions: List[str]):
+def check_permissions_old(permissions: List[str]):
     def decorator(func):
         @wraps(func)
         async def wrapper(request: Request, *args, **kwargs):
--- main.py
+++ main.py
@@ -2,16 +2,36 @@ from fastapi import FastAPI, HTTPException
 from fastapi.openapi.utils import get_openapi
 from fastapi.security import HTTPBearer
 
-from middlewares.exception_handler import http_exception_handler
-from middlewares.token_middleware import TokenVerificationMiddleware
+from shared.config import init_config
+from config.config import API_NAME, URL_API_GATEWAY, KEYCLOAK_HOST, KEYCLOAK_REALM, KEYCLOAK_CLIENT_ID, \
+    KEYCLOAK_CLIENT_SECRET, UNLICENSED_PATHS, UNPROTECTED_PATHS, REDIS_HOST, REDIS_PORT, REDIS_DB, REDIS_PASSWORD
+from shared.middlewares.v0.exception_handler import http_exception_handler
+from shared.middlewares.v0.token_middleware import TokenVerificationMiddleware
 from middlewares.database_middleware import DBConnectionMiddleware
-from middlewares.licence_middleware import LicenceVerificationMiddleware
-from middlewares.cors_middleware import CORSMiddleware
+from shared.middlewares.v0.licence_middleware import LicenceVerificationMiddleware
+from shared.middlewares.v0.cors_middleware import CORSMiddleware
 from routers import v1
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logging.info("Starting API")
+
+logging.info("Loading Config for shared")
+init_config(
+    api_name=API_NAME,
+    url_api_gateway=URL_API_GATEWAY,
+    keycloak_host=KEYCLOAK_HOST,
+    keycloak_realm=KEYCLOAK_REALM,
+    keycloak_client_id=KEYCLOAK_CLIENT_ID,
+    keycloak_client_secret=KEYCLOAK_CLIENT_SECRET,
+    unlicensed_path=UNLICENSED_PATHS,
+    unprotected_path=UNPROTECTED_PATHS,
+    redis_host=REDIS_HOST,
+    redis_db=REDIS_DB,
+    redis_port=REDIS_PORT,
+    redis_password=REDIS_PASSWORD
+)
+logging.info("End loading Config for shared")
 
 bearer_scheme = HTTPBearer()
 
@@ -48,9 +68,10 @@ def custom_openapi():
 app.openapi = custom_openapi
 
 app.add_middleware(DBConnectionMiddleware)
-app.add_middleware(HTTPException, http_exception_handler)
 app.add_middleware(LicenceVerificationMiddleware)
 app.add_middleware(TokenVerificationMiddleware)
 app.add_middleware(CORSMiddleware)
+
+app.add_exception_handler(HTTPException, http_exception_handler)
 
 app.include_router(v1.router)
